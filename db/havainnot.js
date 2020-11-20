475962b29b6714d0e6d1f0174b3dcf21efc6e0f6
ilona <ilona.engblom@tuta.io>
2020-11-20 13:23:02 +0200

    SQL yhdistetty

---
+++ db/havainnot.js
@@ -0,0 +1,89 @@
+const db = require('./dbconfig')
+
+// Hae kaikki havainnot
+const haeHavainnot = (req, res) => {
+    db.query('SELECT * FROM havainnot', (err, result) => {
+        if (err)
+            {console.error(err)}
+        else
+            {res.json(result.rows)}
+    })
+}
+
+// Hae havainto lajin perusteella
+const haeHavainto = (req, res) => {
+    const query = {
+        text: 'SELECT * FROM havainnot WHERE laji = $1',
+        values: [req.params.laji],
+    }
+
+    db.query(query, (err, result) => {
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        }
+        else {
+            if (result.rows.length > 0) {
+                res.json(result.rows)
+            }
+            else {
+                res.status(404).end()
+            }
+        }
+    })
+}
+
+// Lisää havainto
+const lisaaHavainto = (req, res) => {
+    const uusiHavis = req.body
+
+    const query = {
+        text: 'INSERT INTO havainnot (pvm, laji, paikka) VALUES ($1, $2, $3)',
+        values: [uusiHavis.pvm, uusiHavis.laji, uusiHavis.paikka],
+    }
+
+    db.query(query, (err, res) => {
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        }
+    })
+    res.json(uusiHavis)
+}
+
+// poista havainto
+const poistaHavainto = (req, res) => {
+    const query = {
+        text: 'DELETE FROM havainnot WHERE lisatty = $1',
+        values: [req.params.lisatty],
+    }
+
+    db.query(query, (err, res) => {
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        }
+    })
+    res.status(204).end()
+}
+
+// Muokkaa havaintoa
+const paivitaHavainto = (req, res) => {
+    const muokattuHavis = req.body
+    const query = {
+        text: 'UPDATE havainnot SET pvm = $1, laji = $2, paikka = $3 WHERE lisatty = $4',
+        values: [muokattuHavis.pvm, muokattuHavis.laji, muokattuHavis.paikka, req.params.lisatty]
+    }
+
+    db.query(query, (err, res) => {
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        }
+    })
+    res.json(muokattuHavis)
+}
+
+module.exports = {
+    haeHavainnot: haeHavainnot,
+    haeHavainto: haeHavainto,
+    lisaaHavainto: lisaaHavainto,
+    poistaHavainto: poistaHavainto,
+    paivitaHavainto: paivitaHavainto
+}
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const app = express()
 const bodyparser = require('body-parser')
+const query = require('./db/havainnot')
 
 const port = 3000
 
+const app = express()
 app.use(bodyparser.json())
 
 let havikset = [
@@ -11,37 +12,12 @@ let havikset = [
     {lisatty: '13', pvm: '15.11.2020', laji: 'kuningaskalastaja', paikka: 'Halinen'}
 ]
 
-// Hae kaikki havainnot
-app.get("/api/havainnot", (req, res) => {
-    res.json(havikset)
-})
 
-// Lisää havainto
-app.post('/api/havainnot', (req, res) => {
-    const havis = {'lisatty': Date.now(), ...req.body}
-    havikset = [havis, ...havikset]
-
-    res.json(havis)
-})
-
-// Poista havainto
-app.delete('/api/havainnot/:lisatty', (req, res) => {
-    const lisatty = req.params.lisatty
-    havikset = havikset.filter(havis => havis.lisatty != lisatty)
-    res.status(204).end()
-})
-
-// Päivitä havainto
-app.put('/api/havainnot/:lisatty', (req, res) => {
-    const lisatty = req.params.lisatty
-    const paivitettyHavis = {'lisatty': lisatty, ...req.body}
-
-    const index = havikset.findIndex(havis => havis.lisatty === lisatty)
-    havikset.splice(index, 1, paivitettyHavis)
-
-    res.json(paivitettyHavis)
-
-})
+app.get("/api/havainnot", query.haeHavainnot)
+app.get("/api/havainto", query.haeHavainto)
+app.post('/api/havainnot', query.lisaaHavainto)
+app.delete('/api/havainnot/:lisatty', query.poistaHavainto)
+app.put('/api/havainnot/:lisatty', query.paivitaHavainto)
 
 app.listen(process.env.PORT || port, () => {
     console.log(`App running on port {port}`)
