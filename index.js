8bf898c2d7f3db91aec37e6218b86566f50abf9b
ilona <ilona.engblom@tuta.io>
2020-12-28 13:49:17 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const query = require('./db/havainnot')
+const JwksRsa = require('jwks-rsa')
+const jwt = require('express-jwt')
 
 const port = 3000
 
 const app = express()
 app.use(bodyparser.json())
+
+const checkJwt = jwt({
+    secret: JwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://dev-sc8jdyu7.eu.auth0.com/.well-known/jwks.json`
+    }),
+    audience: 'https://havaintopaivakirja-api',
+    issuer: `https://dev-sc8jdyu7.eu.auth0.com/`,
+    algorithms: ['RS256']
+})
+app.use(checkJwt)
+
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
+});
 
 app.get("/api/havainnot", query.haeHavainnot)
 app.get("/api/havainto", query.haeHavainto)
